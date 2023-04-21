07a626327ad29bbf668bf7b233326070e7378be0
Aurelienazerty <Aurelienazerty@users.noreply.github.com>
2023-04-21 11:01:56 +0200

    phpBB 4 compatibility

--- event/listener.php
+++ event/listener.php
@@ -32,6 +32,9 @@ class listener implements EventSubscriberInterface {
 	/** @var \phpbb\event\dispatcher_interface */
 	protected $dispatcher;
 
+    /** @var \phpbb\language\language $language */
+    protected $language;
+
 	/**
 	 * Constructor
 	 *
@@ -39,16 +42,18 @@ class listener implements EventSubscriberInterface {
 	 * @param \phpbb\config\config	$config	Config object
 	 * @param \phpbb\user	$user	user object
 	 * @param \phpbb\template\template $template template object
-	 * \phpbb\event\dispatcher_interface $dispatcher dispatcher object
+	 * @param \phpbb\event\dispatcher_interface $dispatcher dispatcher object
+     * @param \phpbb\language\language $language language object
 	 * @return \aurelienazerty\photos\event\listener
 	 * @access public
 	 */
-	public function __construct(\phpbb\db\driver\driver_interface $db, \phpbb\config\config $config, \phpbb\user $user, \phpbb\template\template $template, \phpbb\event\dispatcher_interface $dispatcher) {
+	public function __construct(\phpbb\db\driver\driver_interface $db, \phpbb\config\config $config, \phpbb\user $user, \phpbb\template\template $template, \phpbb\event\dispatcher_interface $dispatcher, \phpbb\language\language $language) {
 		$this->user = $user;
 		$this->config = $config;
 		$this->db = $db;
 		$this->template = $template;
 		$this->dispatcher = $dispatcher;
+        $this->language = $language;
 	}
 
 	static public function getSubscribedEvents() {
@@ -85,7 +90,7 @@ class listener implements EventSubscriberInterface {
 		$request = new \phpbb\request\request();
 		$request->enable_super_globals();
 		$user_id = $this->user->data["user_id"];
-		$this->user->add_lang_ext('aurelienazerty/photos', 'photos');
+        $this->language->add_lang('photos', 'aurelienazerty/photos');
 		$lesphotos = fonctionGetLastCommentaireForUser($user_id, 10, true);
 		$lesphotos = commentaireForUser($lesphotos, $user_id);
 		
@@ -99,8 +104,14 @@ class listener implements EventSubscriberInterface {
 				$contexte = $photo['contexte'];
 				$indexContexte = sizeof($contexte) - 2;
 				
+				if (isset($photo['user_colour'])) {
+					$user_colour = $photo['user_colour'];
+				} else {
+					$user_colour = '';
+				}
+				
 				$tpl_ary = array(
-					'LAST_POST_AUTHOR_FULL'	=> get_username_string('full', $photo['user_id'], $photo['username'], $photo['user_colour']),
+					'LAST_POST_AUTHOR_FULL'	=> get_username_string('full', $photo['user_id'], $photo['username'], $user_colour),
 					'U_VIEW_PHOTO'					=> $photo['link'],
 					'PHOTO_TITLE' 					=> $photo['nom'],
 					'LAST_POST_TIME'				=> $this->user->format_date($photo['date']),
@@ -109,6 +120,7 @@ class listener implements EventSubscriberInterface {
 					'NEWEST_POST_IMG'				=> $this->user->img('icon_topic_newest', 'VIEW_NEWEST_POST'),
 					'PHOTO_CONTEXTE_URL'		=> replace_mod_rewrite($contexte[$indexContexte]['href']),
 					'PHOTO_CONTEXTE'				=> $contexte[$indexContexte]['txt'],
+					'LAST_COM_READ'					=> $photo['lastComRead'],
 				);
 				
 				//$vars = array('photo', 'tpl_ary');
